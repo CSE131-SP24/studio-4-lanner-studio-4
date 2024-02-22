3c94b7d453dddc3f90c01cf92a6befb26c913aee
kemberlynertulus <kemberlynertulus@localhost>
2024-02-22 11:17:09 -0600

    hey

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,14 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
-	}
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(0.5, 0.5, 0.4, 0.2);
+		
+		StdDraw.setPenColor(Color.GREEN);
+		StdDraw.filledRectangle(.5, 0.5, 0.4, 0.1);
+		
+		StdDraw.setPenColor(Color.black);
+		StdDraw.filledCircle(0.5, 0.5, 0.1);
+	  
+			} 
 }
